4957531a96b6dbf19ffa7625994d24346982a3aa
jarlaxle-536 <eugenegarusov@gmail.com>
2020-09-29 17:40:58 +0300

    added main page & navbar

--- binary_broker/binary_broker/applications/main/views.py
+++ binary_broker/binary_broker/applications/main/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def main(request):
-    return HttpResponse('Main page')
+    template = loader.get_template('main_page.html')
+    context = dict()
+    return HttpResponse(template.render(context, request))
--- binary_broker/binary_broker/settings.py
+++ binary_broker/binary_broker/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'binary_broker.applications.main.apps.MainConfig'
 ]
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'binary_broker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
