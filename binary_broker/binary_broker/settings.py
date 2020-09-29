c6d9c8cb5ca41011cc268c2f78792186b7dcc0f3
jarlaxle-536 <eugenegarusov@gmail.com>
2020-09-29 17:23:15 +0300

    Started main app

--- binary_broker/binary_broker/settings.py
+++ binary_broker/binary_broker/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'binary_broker.applications.main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
